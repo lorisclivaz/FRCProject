9cc069d25a0820696c3637e936a9715765bd61b5
Clivaz Loris <loris.clivaz@hes-so.ch>
2020-03-10 11:59:57 +0000

    Adding php

--- test.php
+++ test.php
@@ -1,3 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Lettres modèles</title>
+</head>
+<body>
+
+<h1>My first PHP page</h1>
+
 <?php
 echo "Hello World!";
 ?>
+
+
+</body>
+</html>
