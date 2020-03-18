09c8426f16dcf57474d603a1936faef824ea4266
Walliser-85 <39823945+Walliser-85@users.noreply.github.com>
2020-03-18 14:42:44 +0100

    Database Connection

--- Php/connection.php
+++ Php/connection.php
@@ -31,56 +31,45 @@ function connection(){
 
 <!-- Aviation -->
 <?php
-
-include "../vendor/autoload.php";
-
-if (isset($_POST['enregistrement']))
+function aviation()
 {
-    $nom = $_POST['lname'];
-    $prenom = $_POST['fname'];
-    $adresse = $_POST['adresse'];
-    $codePostal = $_POST['codepostal'];
-    $nameCompanie = $_POST['nameCompany'];
-    $adresseCompagnie = $_POST['adresseCompany'];
-    $codePostalCompagnie = $_POST['codePostalCompany'];
-    $lieu = $_POST['lieu'];
-    $dateAchat = $_POST['dateAchat'];
-    $destination = $_POST['destination'];
-    $chiffrePerte = $_POST['chiffrePerte'];
-    $iban = $_POST['iban'];
+    include "../vendor/autoload.php";
+
+    if (isset($_POST['enregistrement'])) {
+        $nom = $_POST['lname'];
+        $prenom = $_POST['fname'];
+        $adresse = $_POST['adresse'];
+        $codePostal = $_POST['codepostal'];
+        $nrAssure = $_POST['nrAssure'];
+        $nameCompanie = $_POST['nameCompany'];
+        $adresseCompagnie = $_POST['adresseCompany'];
+        $codePostalCompagnie = $_POST['codePostalCompany'];
+        $lieu = $_POST['lieu'];
 
 
-
-    print "mon nom : $nom" ;
-    print "mon prenom : $prenom";
-    print "mon adresse : $adresse";
-    print "mon code postal: $codePostal";
-    print "mon nom de companie : $nameCompanie";
-    print "mon adresse de companie : $adresseCompagnie";
-    print "mon code postal de companie : $codePostalCompagnie";
-    print "mon lieu  : $lieu";
-    print "mon date achat: $dateAchat";
-    print "mon destination: $destination";
-    print "mon chiffre la perte: $chiffrePerte";
-    print "mon IBAN: $iban";
+        print "mon nom : $nom";
+        print "mon prenom : $prenom";
+        print "mon adresse : $adresse";
+        print "mon code postal: $codePostal";
+        print "mon nrAssure : $nrAssure";
+        print "mon nom de companie : $nameCompanie";
+        print "mon adresse de companie : $adresseCompagnie";
+        print "mon code postal de companie : $codePostalCompagnie";
+        print "mon lieu  : $lieu";
 
 
-    $templateProcessor = new PhpOffice\PhpWord\TemplateProcessor('../Template/Aviation.docx');
+        $templateProcessor = new PhpOffice\PhpWord\TemplateProcessor('../Template/Aviation.docx');
 
-    $templateProcessor->setValue('nom', $nom);
-    $templateProcessor->setValue('prenom', $prenom);
-    $templateProcessor->setValue('rue', $adresse);
-    $templateProcessor->setValue('codepostal', $codePostal);
-    $templateProcessor->setValue('nomSociete', $nameCompanie);
-    $templateProcessor->setValue('adresseSociete', $adresseCompagnie);
-    $templateProcessor->setValue('codePostalSociete', $codePostalCompagnie);
-    $templateProcessor->setValue('lieu', $lieu);
-    $templateProcessor->setValue('dateAchat', $dateAchat);
-    $templateProcessor->setValue('destination', $destination);
-    $templateProcessor->setValue('chiffrePerte', $chiffrePerte);
-    $templateProcessor->setValue('iban', $iban);
+        $templateProcessor->setValue('nom', $nom);
+        $templateProcessor->setValue('prenom', $prenom);
+        $templateProcessor->setValue('rue', $adresse);
+        $templateProcessor->setValue('codepostal', $codePostal);
+        $templateProcessor->setValue('nAssure', $nrAssure);
+        $templateProcessor->setValue('nomSociete', $nameCompanie);
+        $templateProcessor->setValue('adresseSociete', $adresseCompagnie);
+        $templateProcessor->setValue('codePostalSociete', $codePostalCompagnie);
+        $templateProcessor->setValue('lieu', $lieu);
 
+    }
 }
-
-
 ?>
