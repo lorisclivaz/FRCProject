2f1c02ccf41f3134b1f072c7c4a1f8a9cc521bf9
lorisclivaz <loris.clivaz@students.hevs.ch>
2020-03-22 21:22:46 +0100

    Finition du telechargement du template + création de la nouvelle page pour le download

--- Php/index.php
+++ Php/index.php
@@ -6,6 +6,18 @@
     <link rel="stylesheet" type="text/css" href="../css/style.css" media="screen" />
     <title>Lettres modèles</title>
     <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js" type="text/javascript"></script>
+    <style>
+        button
+        {
+            background-color: #e4032e;
+            border: none;
+            color: white;
+            padding: 16px 32px;
+            text-decoration: none;
+            margin: 4px 2px;
+            cursor: pointer;
+        }
+    </style>
     <script>
         function getCat(val) {
             $.ajax({
@@ -72,6 +84,26 @@
                 dd.nextSibling.remove();
         }
 
+        // Function to download data to a file
+        function download(data, filename, type) {
+            var file = new Blob([data], {type: type});
+            if (window.navigator.msSaveOrOpenBlob) // IE10+
+                window.navigator.msSaveOrOpenBlob(file, filename);
+            else { // Others
+                var a = document.createElement("a"),
+                    url = URL.createObjectURL(file);
+                a.href = url;
+                a.download = filename;
+                document.body.appendChild(a);
+                a.click();
+                setTimeout(function() {
+                    document.body.removeChild(a);
+                    window.URL.revokeObjectURL(url);
+                }, 0);
+            }
+        }
+
+
 
     </script>
 </head>
@@ -81,8 +113,9 @@
     </header>
     <main>
         <h1>Lettres modèles</h1>
+
         <br/>
-        <form method="post" name="formular" action="changeValueTemplate.php">
+        <form method="post" name="formular">
             <label>Sélectionnez une catégorie</label>
             <br/>
             <select id="categories" onchange="getCat(this.value);"><option value=" ">Sélectionnez une catégorie</option>
@@ -99,7 +132,7 @@
             <div id="firstQuestion"></div>
         </form>
         <div id="cache" onload="cache(this)" style="display: none">
-            <form enctype="multipart/form-data" action="changeValueTemplate.php" method="post">
+            <form enctype="multipart/form-data"  method="post">
                 <br>
                 <label>Entrez votre nom:</label>
                 <br>
@@ -141,15 +174,74 @@
                 <br>
                 <input id="problematique" name="problematique" type="text" style="display: none"/>
                 <br>
-                <input name="Enregistrement" type="submit" value="Enregistrement" />
+
+                <button name="Enregistrement" type="submit">Créer la lettre</button>
+
+
+
             </form>
         </div>
     </main>
-    <footer>
         <p>© 2020 by FRC-Lausanne</p>
     </footer>
 </body>
 </html>
+
+<?php
+
+include "../vendor/autoload.php";
+
+
+
+//Récupération des valaeurs pour le changement du template
+
+if (isset($_POST['Enregistrement'])) {
+
+    $nom = $_POST['nom'];
+    $prenom = $_POST['prenom'];
+    $rue = $_POST['rue'];
+    $n_rue=$_POST['n°_rue'];
+    $lieu_codepostal = $_POST['lieu_codepostal'];
+
+    $nom_societe = $_POST['nom_societe'];
+    $adresse_societe_n = $_POST['adresse_societe_n°'];
+    $lieu_codepostal_societe = $_POST['lieu_codepostal_societe'];
+    $lieu_envoie = $_POST['lieu_envoie'];
+
+    $paragraph = $_POST['paragraphe_conditionnel'];
+    $problematique = $_POST['problematique'];
+
+
+
+    //récupération du path mais faire ca dynamiquement
+    $templateProcessor = new PhpOffice\PhpWord\TemplateProcessor(getTemplatePathFromCategorie('Aviation'));
+
+    //Set des valeurs dans le template champs de base
+    $templateProcessor->setValue('nom', $nom);
+    $templateProcessor->setValue('prenom', $prenom);
+    $templateProcessor->setValue('rue', $rue);
+    $templateProcessor->setValue('numero', $n_rue);
+    $templateProcessor->setValue('codepostal', $lieu_codepostal);
+
+
+    $templateProcessor->setValue('nomSociete', $nom_societe);
+    $templateProcessor->setValue('adresseSociete', $adresse_societe_n);
+    $templateProcessor->setValue('codePostalSociete', $lieu_codepostal_societe);
+    $templateProcessor->setValue('lieu', $lieu_envoie);
+
+    $templateProcessor->setValue('paragraphe_conditionnel', $paragraph);
+    $templateProcessor->setValue('problematique', $problematique);
+
+    $templateProcessor->saveAs('../final_template/template.docx');
+
+    echo "<script type='text/javascript'>document.location.replace('download.html');</script>";
+
+    return false;
+
+
+
+}
+?>
 
 
 
