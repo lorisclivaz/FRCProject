9ad73283f14661d57f9ea72243e33cc2041bb17f
Kalja97 <jannis.kalbermatten@students.hevs.ch>
2020-04-01 19:38:00 +0200

    add template + nav

--- Php/backoffice.php
+++ Php/backoffice.php
@@ -4,6 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <link rel="stylesheet" type="text/css" href="../css/style.css" media="screen" />
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
     <title>Back Office</title>
     <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js" type="text/javascript"></script>
     <style>
@@ -43,29 +44,51 @@
 
     <br/>
 
-    <form action="categorie.php" method="get">
-        <input type="submit" value="Ajouter une catégorie">
-    </form>
-    <br>
-    <form action="template.php" method="get">
-        <input type="submit" value="Ajouter un nouveau template">
-    </form>
-    <br>
-    <form action="model.php" method="get">
-        <input type="submit" value="Ajouter un nouveau model">
-    </form>
-    <br>
-    <form action="question.php" method="get">
-        <input type="submit" value="Ajouter une nouvelle question">
-    </form>
-    <br>
-    <form action="paragraphe.php" method="get">
-        <input type="submit" value="Ajouter un nouveau paragraphe">
-    </form>
-    <br>
-    <form action="champs.php" method="get">
-        <input type="submit" value="Ajouter de nouveau champs">
-    </form>
+    <div class="container-fluid">
+        <div class="row">
+            <nav class="col-md-5 d-none d-md-block bg-light sidebar">
+                <div class="sidebar-sticky">
+                    <ul class="nav flex-column">
+                        <li class="nav-item">
+                            <a class="nav-link active" href="categorie.php">
+                                <span data-feather="home"></span>
+                                Ajouter une catégorie <span class="sr-only">(current)</span>
+                            </a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="template.php">
+                                <span data-feather="file"></span>
+                                Ajouter un nouveau template
+                            </a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="model.php">
+                                <span data-feather="shopping-cart"></span>
+                                Ajouter un nouveau model
+                            </a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="question.php">
+                                <span data-feather="users"></span>
+                                Ajouter une nouvelle question
+                            </a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="paragraphe.php">
+                                <span data-feather="bar-chart-2"></span>
+                                Ajouter un nouveau paragraphe
+                            </a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="champs.php">
+                                <span data-feather="layers"></span>
+                                Ajouter de nouveau champs
+                            </a>
+                        </li>
+                    </ul>
+                </div>
+            </nav>
+
 </main>
 <p>© 2020 by FRC-Lausanne</p>
 </footer>
--- Php/template.php
+++ Php/template.php
@@ -4,6 +4,8 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <link rel="stylesheet" type="text/css" href="../css/style.css" media="screen" />
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+
     <title>Back Office</title>
     <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js" type="text/javascript"></script>
     <style>
@@ -39,7 +41,38 @@
     <img src="../images/logo.png" alt="logo">
 </header>
 <main>
-    <h1>Ajouter un nouveau model</h1>
+
+    <div class="container-fluid">
+        <div class="row">
+            <div class="card col-md-3">
+                <ul class="list-group list-group-flush">
+                    <a href="categorie.php"><li class="list-group-item">Ajouter une catégorie</li></a>
+                    <a href="template.php"><li class="list-group-item">Ajouter un nouveau template</li></a>
+                    <a href="model.php"><li class="list-group-item">Ajouter un nouveau model</li></a>
+                    <a href="question.php"><li class="list-group-item">Ajouter une nouvelle question</li></a>
+                    <a href="paragraphe.php"><li class="list-group-item">Ajouter un nouveau paragraphe</li></a>
+                    <a href="champs.php"><li class="list-group-item">Ajouter de nouveau champs</li></a>
+                </ul>
+            </div>
+
+            <div class="card col-md-9">
+                <div class="card-body">
+                    <h1>Ajouter un nouveau template</h1>
+                    <br>
+                    <form enctype="multipart/form-data" action="__URL__" method="POST">
+                        <input type="hidden" name="MAX_FILE_SIZE" value="30000" />
+                        Upload the template: <input name="userfile" type="file" />
+                        <br>
+                        <br>
+                        <input type="submit" value="Send File" />
+                    </form>
+                </div>
+            </div>
+        </div>
+    </div>
+
+
+
 
 </main>
 <p>© 2020 by FRC-Lausanne</p>
