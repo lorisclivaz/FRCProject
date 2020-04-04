a7103c8644653197e94ca9f261eafae06536c532
David <david.felley@students.hevs.ch>
2020-04-05 01:29:26 +0200

    Preview split screen is good

--- Php/connection.php
+++ Php/connection.php
@@ -3,7 +3,8 @@
 $conn = null;
 
 //Doing sql connection
-function connection(){
+function connection()
+{
     $servername = "krmg.myd.infomaniak.com";
     $username = "krmg_lm2020";
     $password = "JOkFkZdgk3cE";
@@ -20,15 +21,16 @@ function connection(){
 }
 
 
-function categorieListBO(){
+function categorieListBO()
+{
     $conn = connection();
     $sql = "SELECT idcategories, categoriename FROM categories";
     $result = $conn->query($sql);
 
     if ($result->num_rows > 0) {
         // output data of each row
-        while($row = $result->fetch_assoc()) {
-            echo "<option value=".$row["categoriename"].">".$row["categoriename"]."</option>";
+        while ($row = $result->fetch_assoc()) {
+            echo "<option value=" . $row["categoriename"] . ">" . $row["categoriename"] . "</option>";
         }
     } else {
         echo "<option>Aucune catégories</option>";
@@ -36,15 +38,16 @@ function categorieListBO(){
     $conn->close();
 }
 
-function categorieList(){
+function categorieList()
+{
     $conn = connection();
     $sql = "SELECT idcategories, categoriename FROM categories";
     $result = $conn->query($sql);
 
     if ($result->num_rows > 0) {
         // output data of each row
-        while($row = $result->fetch_assoc()) {
-            echo "<option value=".$row["idcategories"].">".$row["categoriename"]."</option>";
+        while ($row = $result->fetch_assoc()) {
+            echo "<option value=" . $row["idcategories"] . ">" . $row["categoriename"] . "</option>";
         }
     } else {
         echo "<option>Aucune catégories</option>";
@@ -52,7 +55,8 @@ function categorieList(){
     $conn->close();
 }
 
-function addCategorie($categoriename){
+function addCategorie($categoriename)
+{
     $conn = connection();
     $sql = "INSERT INTO categories (categoriename) VALUES ('$categoriename')";
     if ($conn->query($sql) === TRUE) {
@@ -63,15 +67,16 @@ function addCategorie($categoriename){
     $conn->close();
 }
 
-function modelListBO($idCategorie){
+function modelListBO($idCategorie)
+{
     $conn = connection();
-    $sql = "SELECT * FROM models WHERE categories_idcategories = ".$idCategorie;
+    $sql = "SELECT * FROM models WHERE categories_idcategories = " . $idCategorie;
     $result = $conn->query($sql);
 
     if ($result->num_rows > 0) {
         // output data of each row
-        while($row = $result->fetch_assoc()) {
-            echo "<option value=".$row["name"].">".$row["name"]."</option>";
+        while ($row = $result->fetch_assoc()) {
+            echo "<option value=" . $row["name"] . ">" . $row["name"] . "</option>";
         }
     } else {
         echo "<option>Aucune models</option>";
@@ -79,17 +84,18 @@ function modelListBO($idCategorie){
     $conn->close();
 }
 
-function modelList($idCategorie){
+function modelList($idCategorie)
+{
 
     $conn = connection();
-    $sql = "SELECT * FROM models WHERE categories_idcategories = ".$idCategorie;
+    $sql = "SELECT * FROM models WHERE categories_idcategories = " . $idCategorie;
     $result = $conn->query($sql);
 
     if ($result->num_rows > 0) {
         // output data of each row
         echo "<option> Selectionnez un champ</option>";
-        while($row = $result->fetch_assoc()) {
-            echo "<option value=".$row["idmodels"].">".$row["name"]."</option>";
+        while ($row = $result->fetch_assoc()) {
+            echo "<option value=" . $row["idmodels"] . ">" . $row["name"] . "</option>";
         }
     } else {
         echo "<option>Aucun modèles</option>";
@@ -97,9 +103,10 @@ function modelList($idCategorie){
     $conn->close();
 }
 
-function getCategorieIdByName($categorieName){
+function getCategorieIdByName($categorieName)
+{
     $conn = connection();
-    $sql = "SELECT idcategories FROM categories WHERE categoriename = '".$categorieName."'";
+    $sql = "SELECT idcategories FROM categories WHERE categoriename = '" . $categorieName . "'";
     $result = $conn->query($sql);
 
     if ($result->num_rows > 0) {
@@ -110,7 +117,8 @@ function getCategorieIdByName($categorieName){
     }
 }
 
-function addModel($modelname, $categoriename){
+function addModel($modelname, $categoriename)
+{
     $conn = connection();
     $id = getCategorieIdByName($categoriename);
     $sql = "INSERT INTO models (name, categories_idcategories) VALUES ('$modelname', '$id')";
@@ -124,7 +132,8 @@ function addModel($modelname, $categoriename){
     $conn->close();
 }
 
-function addTemplate($templatename, $path, $categoriename){
+function addTemplate($templatename, $path, $categoriename)
+{
     $conn = connection();
     $id = getCategorieIdByName($categoriename);
     $sql = "INSERT INTO template (name, path, categories_idcategories) VALUES ('$templatename', '$path', '$id')";
@@ -138,7 +147,8 @@ function addTemplate($templatename, $path, $categoriename){
     $conn->close();
 }
 
-function getFirstAnswerAndQuestion($idModel){
+function getFirstAnswerAndQuestion($idModel)
+{
     $conn = connection();
     $sqlQuestions = "SELECT * FROM question WHERE models_idmodels ='$idModel' AND priority = 1";
     $result = $conn->query($sqlQuestions);
@@ -146,7 +156,7 @@ function getFirstAnswerAndQuestion($idModel){
     if ($result->num_rows > 0) {
         $row = $result->fetch_assoc();
         $idQuestion = $row["idquestion"];
-        echo "<span>".$row["question"]." </span>";
+        echo "<span>" . $row["question"] . " </span>";
         getInfosQuestions($row["idquestion"]);
         $sqlAnswers = "SELECT * FROM answers WHERE question_idquestion ='$idQuestion' AND next_question > 0";
         $result = $conn->query($sqlAnswers);
@@ -154,8 +164,8 @@ function getFirstAnswerAndQuestion($idModel){
         echo "<select name='answers[]' onChange='get_next_question(this.value, this);'>";
         echo "<option value='Selectionnez un champ'>Selectionnez un champ</option>";
         // output data of each row
-        while($row = $result->fetch_assoc()) {
-            echo "<option value=".$row["next_question"].">".$row["answer"]."</option>";
+        while ($row = $result->fetch_assoc()) {
+            echo "<option value=" . $row["next_question"] . ">" . $row["answer"] . "</option>";
         }
         echo "</select><br><br>";
     } else {
@@ -164,17 +174,17 @@ function getFirstAnswerAndQuestion($idModel){
     $conn->close();
 }
 
-function getInfosQuestions($idquestion){
+function getInfosQuestions($idquestion)
+{
     $conn = connection();
     $sqlQuestions = "SELECT * FROM explanations WHERE question_idquestion ='$idquestion'";
     $result = $conn->query($sqlQuestions);
 
-    if($result->num_rows > 0){
+    if ($result->num_rows > 0) {
         $row = $result->fetch_assoc();
         $title = $row["text"];
-        echo "<img src='../images/info.png' width='15px' title="."'$title'"." onmouseover='showInfos(this);'><br/>";
-    }
-    else
+        echo "<img src='../images/info.png' width='15px' title=" . "'$title'" . " onmouseover='showInfos(this);'><br/>";
+    } else
         echo "<br/>";
 }
 
@@ -188,26 +198,26 @@ function get_next_question_2($id)
     if ($result->num_rows > 0) {
         $row = $result->fetch_assoc();
         $idQuestion = $row["idquestion"];
-        echo "<span>".$row["question"]." </span>";
+        echo "<span>" . $row["question"] . " </span>";
         getInfosQuestions($row["idquestion"]);
         $sqlAnswers = "SELECT * FROM answers WHERE question_idquestion ='$idQuestion'";
         $result = $conn->query($sqlAnswers);
         echo "<select name='answers[]' onChange='get_next_question(this.value, this);'>";
         echo "<option value='Selectionnez un champ'>Selectionnez un champ</option>";
         // output data of each row
-        while($row = $result->fetch_assoc()) {
-            echo "<option value=".$row["next_question"].">".$row["answer"]."</option>";
+        while ($row = $result->fetch_assoc()) {
+            echo "<option value=" . $row["next_question"] . ">" . $row["answer"] . "</option>";
         }
         echo "</select><br><br>";
-    }
-    else{
+    } else {
         echo "null";
     }
 
     $conn->close();
 }
 
-function getTemplatePathFromCategorie($categorieName){
+function getTemplatePathFromCategorie($categorieName)
+{
 
     $conn = connection();
     $sql = "SELECT path FROM template as t, categories as c WHERE t.categories_idcategories = c.idcategories AND c.categoriename = '$categorieName'";
@@ -224,7 +234,8 @@ function getTemplatePathFromCategorie($categorieName){
     $conn->close();
 }
 
-function get_Paragraph_From_id($id){
+function get_Paragraph_From_id($id)
+{
     $conn = connection();
     $paragraph = "SELECT name FROM paragraphs WHERE number = '$id'";
     $result = $conn->query($paragraph);
@@ -236,24 +247,26 @@ function get_Paragraph_From_id($id){
     $conn->close();
 }
 
-function getFieldFromCategorie($id){
+function getFieldFromCategorie($id)
+{
     $conn = connection();
     $paragraph = "SELECT * FROM fields WHERE id_categorie = '$id'";
     $result = $conn->query($paragraph);
 
     if ($result->num_rows > 0) {
         // output data of each row
-        while($row = $result->fetch_assoc()) {
-            echo "<label>".$row["name"].":</label><br/>";
+        while ($row = $result->fetch_assoc()) {
+            echo "<label>" . $row["name"] . ":</label><br/>";
 
-            echo "<input name=".$row["balise_name"]." type='text'/><br/>";
+            echo "<input name=" . $row["balise_name"] . " type='text'/><br/>";
         }
     }
 
     $conn->close();
 }
 
-function getFieldFromParaNumber($number){
+function getFieldFromParaNumber($number)
+{
     $conn = connection();
     $paragraph = "SELECT f.name, f.balise_name FROM fields f, paragraphs p, paragraphs_fields pf WHERE p.idparagraphs = pf.fk_paragraph AND f.idfield = pf.fk_field AND p.number = '$number'";
 
@@ -261,9 +274,9 @@ function getFieldFromParaNumber($number){
 
     if ($result->num_rows > 0) {
         // output data of each row
-        while($row = $result->fetch_assoc()) {
-            echo "<label>".$row["name"].":</label><br/>";
-            echo "<input name=".$row["balise_name"]." type='text'/><br/>";
+        while ($row = $result->fetch_assoc()) {
+            echo "<label>" . $row["name"] . ":</label><br/>";
+            echo "<input name=" . $row["balise_name"] . " type='text'/><br/>";
         }
     }
 
--- Php/index.php
+++ Php/index.php
@@ -2,12 +2,11 @@
 <html lang="en">
 <head>
     <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <link rel="stylesheet" type="text/css" href="../css/style.css" media="screen" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+    <link rel="stylesheet" type="text/css" href="../css/style.css" media="screen"/>
     <title>Lettres modèles</title>
     <style>
-        button
-        {
+        button {
             background-color: #e4032e;
             border: none;
             color: white;
@@ -16,57 +15,76 @@
             margin: 4px 2px;
             cursor: pointer;
         }
+
+        #splitScreen {
+            width: 40%;
+            float: left;
+            margin-right: 10%;
+        }
+
+        #iframeApercu {
+            width: 40%;
+            height: 600px;
+        }
+
+        .footer {
+            position: fixed;
+            left: 0;
+            bottom: 0;
+            width: 100%;
+        }
     </style>
     <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js" type="text/javascript"></script>
     <script src="https://code.jquery.com/ui/1.12.1/jquery-ui.js"></script>
 
     <script>
         let para;
+
         function getCat(val) {
             $.ajax({
                 type: "POST",
                 url: "getInfosFromUser.php",
-                data:'categorie_id='+val,
-                success: function(data){
+                data: 'categorie_id=' + val,
+                success: function (data) {
                     $("#models").html(data);
                 }
             });
             getFieldsFromCat(val);
 
-            document.getElementById("iframeApercu").style.visibility = "visible";
+            let preview = document.getElementById("apercu");
+            preview.style.visibility = "visible";
         }
-        function get_question(val){
+
+        function get_question(val) {
             $.ajax({
                 type: 'POST',
                 url: 'getInfosFromUser.php',
-                data: 'model_id='+val,  
+                data: 'model_id=' + val,
                 success: function (data) {
 
                     $("#firstQuestion").html(data);
 
-                  let test =  document.getElementById("models").options[document.getElementById('models').selectedIndex].text;
+                    let test = document.getElementById("models").options[document.getElementById('models').selectedIndex].text;
 
-                  document.getElementById("problematique").value=test;
+                    document.getElementById("problematique").value = test;
                 }
             });
         }
+
         function get_next_question(val, obj) {
             deleteSibling(obj);
-            if(document.getElementById("cache").style.display === "block")
+            if (document.getElementById("cache").style.display === "block")
                 document.getElementById("cache").style.display = "none"
             $.ajax({
                 type: 'POST',
                 url: 'getInfosFromUser.php',
                 data: 'next_question=' + val,
                 success: function (data) {
-                    if (data === "null")
-                    {
-                        document.getElementById("cache").style.display='block';
+                    if (data === "null") {
+                        document.getElementById("cache").style.display = 'block';
                         getParagraph(val);
                         getFieldsFromParagraph(val);
-                    }
-                    else
-                    {
+                    } else {
                         let elem = document.createElement('div');
                         elem.innerHTML = data;
                         document.getElementsByName('formular')[0].appendChild(elem);
@@ -74,174 +92,158 @@
                 }
             });
         }
+
         function getParagraph(val) {
             $.ajax({
                 type: 'POST',
                 url: 'getInfosFromUser.php',
                 data: 'paragraph_number=' + val,
                 success: function (data) {
-                    para=data;
+                    para = data;
 
                     console.log(para);
                 }
             });
         }
-        function getInputValueforParagraph(){
+
+        function getInputValueforParagraph() {
 
             // Selecting the input element and get its value
-
-
-            if (document.getElementsByName("date_vol").length == 1)
-            {
-                alert("salut");
+            if (document.getElementsByName("date_vol").length == 1) {
                 let date_vol = document.getElementsByName("date_vol").value.text;
-
-                para = para.replace("$[date_vol]",date_vol);
-
+                para = para.replace("$[date_vol]", date_vol);
             }
 
-            if (document.getElementsByName("nbr_heures_retard").length == 1)
-            {
+            if (document.getElementsByName("nbr_heures_retard").length == 1) {
                 let nbr_heures_retard = document.getElementsByName("nbr_heures_retard").value;
-                para = para.replace("$[nbr_heures_retard]",nbr_heures_retard);
-
+                para = para.replace("$[nbr_heures_retard]", nbr_heures_retard);
             }
 
-            if (document.getElementsByName("montant").length == 1)
-            {
-
+            if (document.getElementsByName("montant").length == 1) {
                 let montant = document.getElementsByName("montant").value;
-                para = para.replace("$[montant]",montant);
-
+                para = para.replace("$[montant]", montant);
             }
 
-            document.getElementById("paragraph").value=para;
+            document.getElementById("paragraph").value = para;
         }
-        function getFieldsFromCat(val){
+
+        function getFieldsFromCat(val) {
             $.ajax({
                 type: "POST",
                 url: "getInfosFromUser.php",
-                data:'categorie_field='+val,
-                success: function(data){
+                data: 'categorie_field=' + val,
+                success: function (data) {
 
                     $("#cat_fields").html(data);
                 }
             });
         }
-        function getFieldsFromParagraph(val){
+
+        function getFieldsFromParagraph(val) {
             $.ajax({
                 type: "POST",
                 url: "getInfosFromUser.php",
-                data:'paragraphe_number='+val,
-                success: function(data){
+                data: 'paragraphe_number=' + val,
+                success: function (data) {
                     $("#para_fields").html(data);
                 }
             });
         }
-        function deleteSibling(obj){
+
+        function deleteSibling(obj) {
             let dd = obj.parentNode;
             var ns;
-            while(ns = dd.nextSibling)
+            while (ns = dd.nextSibling)
                 dd.nextSibling.remove();
         }
+
         function showInfos(obj) {
             obj.tooltip();
         }
     </script>
 </head>
 <body>
-    <header>
-        <img src="../images/logo.png" alt="logo">
-    </header>
-    <main>
+<header>
+    <img src="../images/logo.png" alt="logo">
+</header>
+<main>
+    <div id="splitScreen">
         <h1>Lettres modèles</h1><a href="backoffice.php">Back Office</a>
-
         <br/>
-
-        <table>
-            <tr>
-                <td width="1000px" height="10px">
-                    <form method="post" name="formular">
-                        <label>Sélectionnez une catégorie</label>
-                        <br/>
-                        <select id="categories" onchange="getCat(this.value);"><option value=" ">Sélectionnez une catégorie</option>
-                            <?php include "connection.php";
-                            categorieList();
-                            ?>
-                        </select>
-                        <br/><br/>
-                        <label>Sélectionnez un modèle</label>
-                        <br/>
-                        <select  id="models" onchange="get_question(this.value);" ><option value="">Sélectionnez un modèle</option>
-                        </select>
-                        <br/><br/>
-                        <div id="firstQuestion"></div>
-                    </form>
-                    <div id="cache" onload="cache(this)" style="display: none">
-                        <form enctype="multipart/form-data"  method="post">
-                            <br>
-                            <label>Entrez votre nom:</label>
-                            <br>
-                            <input name="nom" type="text"/>
-                            <br>
-                            <label>Entrez votre prénom:</label>
-                            <br>
-                            <input name="prenom" type="text"/>
-                            <br>
-                            <label>Entrez votre rue:</label>
-                            <br>
-                            <input name="rue" type="text"/>
-                            <br>
-                            <label>Entrez votre n° de rue:</label>
-                            <br>
-                            <input name="n°_rue" type="text"/>
-                            <br>
-                            <label>Entrez le lieu et le code postal:</label>
-                            <br>
-                            <input name="lieu_codepostal" type="text"/>
-                            <br>
-                            <label>Entrez le nom de la société:</label>
-                            <br>
-                            <input name="nom_societe" type="text"/>
-                            <br>
-                            <label>Entrez l'adresse et le n° de la société:</label>
-                            <br>
-                            <input name="adresse_societe_n°" type="text"/>
-                            <br>
-                            <label>Entrez le lieu et le code postal de la société:</label>
-                            <br>
-                            <input name="lieu_codepostal_societe" type="text"/>
-                            <br>
-                            <label>Entrez le lieu d'envoie de la lettre:</label>
-                            <br>
-                            <input name="lieu_envoie" type="text"/>
-                            <br>
-                            <div id="cat_fields"></div>
-                            <div id="para_fields"></div>
-                            <button name="Enregistrement" type="submit" onclick="getInputValueforParagraph()">Créer la lettre</button>
-                            <input id="problematique" name="problematique" type="text" style="display: none"/>
-                            <textarea id="paragraph" name="paragraphe_conditionnel" type="text"  rows="25" cols="30" style="display: none"></textarea>
-                        </form>
-                    </div>
-                </td>
-
-                <td style="width: 10px">
-
-                    <div id="iframeApercu" style="visibility: hidden">
-                        <iframe src="../Apercu/AviationApercu.htm"
-                                width='600px' height='600px' frameborder='0'>
-
-                        </iframe>
-                    </div>
-                </td>
-            </tr>
-        </table>
-
-
-
-    </main>
-        <p>© 2020 by FRC-Lausanne</p>
-    </footer>
+        <form method="post" name="formular">
+            <label>Sélectionnez une catégorie</label>
+            <br/>
+            <select id="categories" onchange="getCat(this.value);">
+                <option value=" ">Sélectionnez une catégorie</option>
+                <?php include "connection.php";
+                categorieList();
+                ?>
+            </select>
+            <br/><br/>
+            <label>Sélectionnez un modèle</label>
+            <br/>
+            <select id="models" onchange="get_question(this.value);">
+                <option value="">Sélectionnez un modèle</option>
+            </select>
+            <br/><br/>
+            <div id="firstQuestion"></div>
+        </form>
+        <div id="cache" onload="cache(this)" style="display: none">
+            <form enctype="multipart/form-data" method="post" style="margin-bottom: 50px">
+                <br/>
+                <label>Entrez votre nom:</label>
+                <br/>
+                <input name="nom" type="text"/>
+                <br/>
+                <label>Entrez votre prénom:</label>
+                <br/>
+                <input name="prenom" type="text"/>
+                <br/>
+                <label>Entrez votre rue:</label>
+                <br/>
+                <input name="rue" type="text"/>
+                <br/>
+                <label>Entrez votre n° de rue:</label>
+                <br/>
+                <input name="n°_rue" type="text"/>
+                <br/>
+                <label>Entrez le lieu et le code postal:</label>
+                <br/>
+                <input name="lieu_codepostal" type="text"/>
+                <br/>
+                <label>Entrez le nom de la société:</label>
+                <br/>
+                <input name="nom_societe" type="text"/>
+                <br/>
+                <label>Entrez l'adresse et le n° de la société:</label>
+                <br/>
+                <input name="adresse_societe_n°" type="text"/>
+                <br/>
+                <label>Entrez le lieu et le code postal de la société:</label>
+                <br/>
+                <input name="lieu_codepostal_societe" type="text"/>
+                <br/>
+                <label>Entrez le lieu d'envoie de la lettre:</label>
+                <br/>
+                <input name="lieu_envoie" type="text"/>
+                <br/>
+                <div id="cat_fields"></div>
+                <div id="para_fields"></div>
+                <button name="Enregistrement" type="submit" onclick="getInputValueforParagraph()">Créer la lettre</button>
+                <input id="problematique" name="problematique" type="text" style="display: none"/>
+                <textarea id="paragraph" name="paragraphe_conditionnel" type="text" rows="25" cols="30"
+                          style="display: none"></textarea>
+            </form>
+        </div>
+    </div>
+    <div id="apercu" style="visibility: hidden">
+        <h1>Aperçu de votre lettre</h1>
+        <br/>
+        <iframe id="iframeApercu" src="../Apercu/AviationApercu.htm" frameborder='0'></iframe>
+    </div>
+</main>
+<div class="footer">© 2020 by FRC-Lausanne</div>
+</footer>
 </body>
 </html>
 
@@ -255,7 +257,7 @@ if (isset($_POST['Enregistrement'])) {
     $nom = $_POST['nom'];
     $prenom = $_POST['prenom'];
     $rue = $_POST['rue'];
-    $n_rue=$_POST['n°_rue'];
+    $n_rue = $_POST['n°_rue'];
     $lieu_codepostal = $_POST['lieu_codepostal'];
     $nom_societe = $_POST['nom_societe'];
     $adresse_societe_n = $_POST['adresse_societe_n°'];
@@ -263,16 +265,13 @@ if (isset($_POST['Enregistrement'])) {
     $lieu_envoie = $_POST['lieu_envoie'];
     $paragraph = $_POST['paragraphe_conditionnel'];
     $problematique = $_POST['problematique'];
-    $no_vol= $_POST["no_vol"];
-    $date_achat=$_POST["date_achat"];
-   $ville_destination=$_POST["ville_destination"];
+    $no_vol = $_POST["no_vol"];
+    $date_achat = $_POST["date_achat"];
+    $ville_destination = $_POST["ville_destination"];
 
-    $coor_banque=$_POST["coor_banque"];
+    $coor_banque = $_POST["coor_banque"];
 
-    $perte=$_POST["perte"];
-
-
-
+    $perte = $_POST["perte"];
 
 
     //récupération du path mais faire ca dynamiquement
@@ -296,14 +295,13 @@ if (isset($_POST['Enregistrement'])) {
 
     //Valeur selon la categorie
     $templateProcessor->setValue('no_vol', $no_vol);
-   $templateProcessor->setValue('date_achat', $date_achat);
-   $templateProcessor->setValue('ville_destination', $ville_destination);
-     $templateProcessor->setValue('perte', $perte);
+    $templateProcessor->setValue('date_achat', $date_achat);
+    $templateProcessor->setValue('ville_destination', $ville_destination);
+    $templateProcessor->setValue('perte', $perte);
     $templateProcessor->setValue('coor_banque', $coor_banque);
 
     $templateProcessor->saveAs('../final_template/template.docx');
     $templateProcessor->saveAs('../final_template/template.html');
-
 
 
     echo "<script type='text/javascript'>document.location.replace('download.php');</script>";
